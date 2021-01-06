1ae5927e5b131cc2e6f2b44d99803e280684ad39
Miguel Angel Mendoza Cardenas <miigangls@gmail.com>
2021-01-06 12:26:12 -0500

    add pokemons favorites.

--- src/ui/pages/index.js
+++ src/ui/pages/index.js
@@ -5,20 +5,33 @@ import Pokemons from './pokemon'
 
 function App() {
   const  [useSearch, saveSearch ] = useState({nombre: ''})
+  const  [useFavorites, saveFavorites ] = useState([])
 
-  const onSubmit = (stateName, value) => {
+  const onSearch = (stateName, value) => {
     saveSearch({
         ...useSearch,
         [stateName] : value
     })
   }
 
+  const onFavorites = ( key, value ) => {
+    if(useFavorites[key]) {
+      delete  useFavorites[key]
+      saveFavorites({ ...useFavorites })
+    } else {
+      saveFavorites({
+        ...useFavorites,
+        [key] : value
+      })
+    }
+  }
+
   return (
     <div className="content">
         <div className='input-row'>
-            <label style={{fontSize: 16, fontWeight: 'bold'}}>Nombre</label><Text stateName="nombre" onChange={onSubmit} style={{width:500}} label="Nombre" noFormItem={true} />
+            <label style={{fontSize: 16, fontWeight: 'bold'}}>Nombre</label><Text stateName="nombre" onChange={onSearch} style={{width:500}} label="Nombre" noFormItem={true} />
         </div>
-        <Pokemons />
+        <Pokemons useSearch={useSearch} onFavorites={onFavorites} useFavorites={useFavorites} />
     </div>
   );
 }
